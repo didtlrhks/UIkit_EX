ae29a0eda45fc883172341ad8200a0a441e5379f
sikwan <yysskk99999@naver.com>
2024-10-23 13:29:05 +0900

    textlabel add

--- Uikit_EX/ViewController.swift
+++ Uikit_EX/ViewController.swift
@@ -8,10 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    lazy var myNametextLabel : UILabel = {
+        
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "양시관"
+        label.textAlignment = .center
+        label.backgroundColor = .green
+        
+        return label
+        
+        
+        
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .orange
+        
+        view.addSubview(myNametextLabel)
+        myNametextLabel.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        myNametextLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        myNametextLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        myNametextLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
     }
 
 
